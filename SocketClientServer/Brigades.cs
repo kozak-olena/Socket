d2a57bdd7f7b418317194324635a628e9e826bd8
Olena Kozak <kozak.olena11@gmail.com>
2020-10-05 18:05:39 +0300

    Create class Client, create class Brigades

---
+++ SocketClientServer/Brigades.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketClientServer
+{
+    class Brigades
+    {
+        static Dictionary<int, string> brigadeAndSurnames = new Dictionary<int, string>();
+        public static Dictionary<int, string> CreateBrigades()
+        {
+            brigadeAndSurnames.Add(1, "МЕЛЬНИК");
+            brigadeAndSurnames.Add(1, "БОЙКО ");
+            brigadeAndSurnames.Add(1, "КОВАЛЕНКО");
+            brigadeAndSurnames.Add(1, "КОВАЛЬ");
+            brigadeAndSurnames.Add(2, "ПОЛІЩУК");
+            brigadeAndSurnames.Add(2, "ТКАЧУК");
+            brigadeAndSurnames.Add(2, "МОРОЗ");
+            brigadeAndSurnames.Add(2, "ПЕТРЕНКО");
+            brigadeAndSurnames.Add(3, "РУДЕНКО");
+            brigadeAndSurnames.Add(3, "СЕМЕНЮК");
+            brigadeAndSurnames.Add(3, "КРАВЕЦЬ");
+            brigadeAndSurnames.Add(3, "МАКАРЕНКО");
+            return brigadeAndSurnames;
+        }
+    }
+}
--- SocketClientServer/MyServer.cs
+++ SocketClientServer/MyServer.cs
@@ -12,24 +12,6 @@ namespace SocketClientServer
         static int Port = 5544;
         static string IPAdressOfServer = Console.ReadLine();
 
-        public static Dictionary<int, string> CreateBrigades()
-        {
-            Dictionary<int, string> brigadeAndSurnames = new Dictionary<int, string>();
-            brigadeAndSurnames.Add(1, "МЕЛЬНИК");
-            brigadeAndSurnames.Add(1, "БОЙКО ");
-            brigadeAndSurnames.Add(1, "КОВАЛЕНКО");
-            brigadeAndSurnames.Add(1, "КОВАЛЬ");
-            brigadeAndSurnames.Add(2, "ПОЛІЩУК");
-            brigadeAndSurnames.Add(2, "ТКАЧУК");
-            brigadeAndSurnames.Add(2, "МОРОЗ");
-            brigadeAndSurnames.Add(2, "ПЕТРЕНКО");
-            brigadeAndSurnames.Add(3, "РУДЕНКО");
-            brigadeAndSurnames.Add(3, "СЕМЕНЮК");
-            brigadeAndSurnames.Add(3, "КРАВЕЦЬ");
-            brigadeAndSurnames.Add(3, "МАКАРЕНКО");
-            return brigadeAndSurnames;
-        }
-
         public static Socket CreateSocketForServer()
         {
             IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(IPAdressOfServer), Port);
